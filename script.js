98b4b37555688023704f9f4d2f231c57d3c39a19
Paul Gonzales <paulgonzales@Pauls-Mac-mini.local>
2020-12-12 03:47:04 -0800

    Maps and Sets

--- script.js
+++ script.js
@@ -65,11 +65,6 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
 
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
@@ -155,5 +150,23 @@ movements.forEach(function(move, i, arr){
 });
 */
 
+//MAP
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function(value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+//SET
+const currenciesUnique = new Set([`USD`, `GBP`, `USD`, `EUR`, `EUR`]);
+console.log(currenciesUnique);
+// currenciesUnique.forEach(function(value, key, map) {
+currenciesUnique.forEach(function(value, _, map) { //USE THROW AWAY VALUE INSTEAD
+  console.log(`${value}: ${value}`); //IN SETS, THERE ARE NO KEYS SO THE KEY WOULD EQUAL VALUE
+});
 
 
