2a8e26ab746f9735c2bcce13e629e48b0c473994
Paul Gonzales <paulgonzales@Pauls-Mac-mini.local>
2020-12-13 05:29:25 -0800

    coding challenge 2

--- script.js
+++ script.js
@@ -347,7 +347,7 @@ console.log(withdrawals);
 
 /////////////////////////////////////////////////
 //REDUCE METHOD
-
+/*
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // a - current value, b - number after a, c - index, d - source
 const balance = movements.reduce(function(a, b, c, d){
@@ -364,3 +364,55 @@ const max = movements.reduce((acc, mov) => {
       return mov;
 }, movements[0]);
 console.log(max);
+*/
+
+/////////////////////////////////////////////////
+//CODING CHALLENGE 2
+
+/* 
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog 
+ages to human ages and calculate the average age of the dogs in their study.
+
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), 
+and does the following things in order:
+
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old,
+    humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that
+    are at least 18 years old)
+
+3. Calculate the average human age of all adult dogs (you should already know from other challenges
+    how we calculate averages 😉)
+
+4. Run the function for both test datasets
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+const dogAges = [5, 2, 4, 1, 15, 8, 3];
+
+const calcAverageHumanAge = dogAges.map(function(dogAge){
+
+  if (dogAge <= 2){
+    return dogAge * 2;
+  } else  {
+    return 16 + dogAge * 4;
+  }
+});
+
+console.log(calcAverageHumanAge);
+
+const legalDogs = calcAverageHumanAge.filter(function(humanAge){
+  return humanAge >= 18;
+})
+
+console.log(legalDogs);
+
+const avgAges = legalDogs.reduce(function(a, b){
+  return a + b;
+})
+console.log(avgAges / legalDogs.length);
